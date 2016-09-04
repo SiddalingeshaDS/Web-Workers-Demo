eb3b4db31e230e09ce857ab1f81e59c912c0d1f6
Siddalingesha D S <siddhudev813@gmail.com>
2016-09-04 17:03:47 +0530

    added web worker to handle the image manipulation

--- scripts/image-app.js
+++ scripts/image-app.js
@@ -5,6 +5,15 @@
   imageLoader.addEventListener('change', handleImage, false);
   var canvas = document.querySelector('#image');
   var ctx = canvas.getContext('2d');
+  var worker = new Worker('scripts/worker.js');
+  // adding the event listener to get data from worker
+  worker.addEventListener('message', function(e) {
+      if(e && e.data ){
+        ctx.putImageData(e.data, 0, 0);
+      }else{
+        console.log("error");
+      }
+    }, false);
 
   function handleImage(e){
     var reader = new FileReader();
@@ -42,8 +51,11 @@
 
     // Hint! This is where you should post messages to the web worker and
     // receive messages from the web worker.
-
-    length = imageData.data.length / 4;
+    
+     worker.postMessage({'imageData': imageData, 'type': type});
+    
+//    imageData = 
+/*    length = imageData.data.length / 4;
     for (i = j = 0, ref = length; 0 <= ref ? j <= ref : j >= ref; i = 0 <= ref ? ++j : --j) {
       r = imageData.data[i * 4 + 0];
       g = imageData.data[i * 4 + 1];
@@ -55,8 +67,9 @@
       imageData.data[i * 4 + 2] = pixel[2];
       imageData.data[i * 4 + 3] = pixel[3];
     }
+  */
     toggleButtonsAbledness();
-    return ctx.putImageData(imageData, 0, 0);
+    return;
   };
 
   function revertImage() {
